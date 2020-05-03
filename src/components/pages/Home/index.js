b860b7e9f93d35903e6a895370d5e3798ac035b0
Leandra Turner <calliet0715@gmail.com>
2020-05-03 15:49:04 -0500

    Finished Home page

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+    return (
+        <div className="hero-image" id="home">
+         <div className="row">
+                <div className="hero-text">
+                    <h3>Hello my name is</h3>
+                    <h1 className="name">Leandra Turner</h1>
+                    <h3> I am a Full Stack Developer</h3>
+                </div>
+            </div>
+            <div className="row">
+            </div>
+        </div>
+    )
+}
+
+export default Home;
