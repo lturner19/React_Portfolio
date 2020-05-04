10213e155dcd559693a25ee0d09351ebe7a5e571
Leandra Turner <calliet0715@gmail.com>
2020-05-04 07:20:00 -0500

    no changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 import NavBar from './components/NavBar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import Projects from './components/pages/Projects';
+import Portfolio from './components/pages/Portfolio';
 import Contact from './components/pages/Contact';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -16,7 +16,7 @@ function App() {
       {/* this ensures that either / or home can be used to bring up homepage w/o breaking*/}
       <Route exact path={["/", "/home"]}><Home /></Route>
       <Route exact path="/about"><About/></Route>
-      <Route exact path="/projects"><Projects /></Route>
+      <Route exact path="/portfolio-projects"><Portfolio/></Route>
       <Route exact path="/contact"><Contact/></Route>
       </Switch>
       <Footer />
