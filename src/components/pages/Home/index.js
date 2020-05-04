b5fecb45e183be0977cc644bb16cfb6a6b17f405
Leandra Turner <calliet0715@gmail.com>
2020-05-04 06:30:03 -0500

    replacing grid with react-mdl grid and styling

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Home.css';
+import { Grid, Cell } from 'react-mdl'
 
 
 function Home() {
     return (
-        <div className="hero-image" id="home">
-         <div className="row">
-                <div className="hero-text">
-                    <h3>Hello my name is</h3>
-                    <h1 className="name">Leandra Turner</h1>
-                    <h3> I am a Full Stack Developer</h3>
-                </div>
-            </div>
-            <div className="row">
-            </div>
+        <div className="main">
+            <Grid className="home-grid">
+                <Cell col={12}>
+                    <div className="banner-text">
+                        <h3>Hi my name is</h3>
+                        <h1>Leandra Turner</h1>
+                        <h3> I am a Full Stack Developer</h3>
+                        <hr />
+                        <p>HTML/CSS | Bootstrap | JavaScript | Vue.js | React.js | Node.js | MongoDB | mySQL</p>
+                    </div>
+                </Cell>
+            </Grid>
         </div>
     )
 }
