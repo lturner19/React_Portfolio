e83a4dc7b16064e28e297b2a62bb6f99b5332d41
Leandra Turner <calliet0715@gmail.com>
2020-05-04 21:46:09 -0500

    Changing portfolio link to project link

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './index.css';
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import Portfolio from './components/pages/Portfolio';
+import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+
+
 function App() {
   return (
     <Router>  
-      <Header />
+      <Header/>
       <Switch>
       {/* this ensures that either / or home can be used to bring up homepage w/o breaking*/}
       <Route exact path={["/", "/home"]}><Home /></Route>
       <Route exact path="/about"><About/></Route>
-      <Route exact path="/portfolio-projects"><Portfolio/></Route>
+      <Route exact path="/portfolio-projects"><Projects/></Route>
       <Route exact path="/contact"><Contact/></Route>
       </Switch>
       <Footer />
