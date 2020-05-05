9a5532efe8d4b8dd7f675c14bdbb231652d59051
Leandra Turner <calliet0715@gmail.com>
2020-05-04 21:45:03 -0500

    Updating styling for nav header and links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,23 +7,23 @@ import { Layout, Header, Navigation, Drawer } from 'react-mdl';
 function NavBar() {
     return (
         <div >
-            <Layout >
-                <Header className="header" title="Portfolio" scroll>
+            <Layout>
+                <Header className="nav-header" title="Portfolio" scroll>
                     <Navigation className="main-nav">
                         <Link className="nav" to="/">Home</Link>
                         <Link className="nav" to="/about">About</Link>
                         <Link className="nav" to="/portfolio-projects">Projects</Link>
                         <Link className="nav" to="/contact">Contact</Link>
-                        <a className="nav" href="/LT_2020_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+                        <Link className="nav" to="/LT_2020_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</Link>
                     </Navigation>
                 </Header>
-                <Drawer title="Portfolio">
+                <Drawer className="nav-header" title="Portfolio">
                     <Navigation>
                         <Link className="nav" to="/">Home</Link>
                         <Link className="nav" to="/about">About</Link>
                         <Link className="nav" to="/portfolio-projects">Projects</Link>
                         <Link className="nav" to="/contact">Contact</Link>
-                        <a className="nav" href="/LT_2020_Resume.pdf" target="_blank">Resume</a>
+                        <Link className="nav" to="/LT_2020_Resume.pdf" target="_blank">Resume</Link>
                     </Navigation>
                 </Drawer>
             </Layout>
